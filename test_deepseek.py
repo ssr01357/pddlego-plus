4f6ed2f7660cbc38d1a8e747e7c9e6cfa9b18958
krystalgong <43835392+krystalgong@users.noreply.github.com>
2025-04-06 21:55:43 -0400

    test deepseek api

---
+++ test_deepseek.py
@@ -0,0 +1,14 @@
+from openai import OpenAI
+
+client = OpenAI(api_key="sk-a3a8546a60d949148e7dc20a68eaf07e", base_url="https://api.deepseek.com")
+
+response = client.chat.completions.create(
+    model="deepseek-chat",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant"},
+        {"role": "user", "content": "Hello"},
+    ],
+    stream=False
+)
+
+print(response.choices[0].message.content)
